0ecc9dd26bacf99e33496b116f101c386334b2e7
bg <bg@Buraks-MacBook-Pro.local>
2021-02-18 23:50:03 +0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAoIxNKlAmHXTLc8UNbgjfIeA-bJFLbvz8",
+    authDomain: "member-camlibel.firebaseapp.com",
+    projectId: "member-camlibel",
+    storageBucket: "member-camlibel.appspot.com",
+    messagingSenderId: "1045033837216",
+    appId: "1:1045033837216:web:fab8cc196ad87e39fb9fc5",
+    measurementId: "G-WXE0R50T2R"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAoIxNKlAmHXTLc8UNbgjfIeA-bJFLbvz8",
+    authDomain: "member-camlibel.firebaseapp.com",
+    projectId: "member-camlibel",
+    storageBucket: "member-camlibel.appspot.com",
+    messagingSenderId: "1045033837216",
+    appId: "1:1045033837216:web:fab8cc196ad87e39fb9fc5",
+    measurementId: "G-WXE0R50T2R"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
